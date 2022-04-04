2857203be0626003907aa246068e0d63d1d16fc0
Patchkat <24392778+Patchkat@users.noreply.github.com>
2022-04-04 16:03:48 -0600

    Create templater.user.js

---
+++ templater.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         U of U and Fnatic Template
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  try to take over the canvas!
+// @author       oralekin, exdeejay (xDJ_), Patchkat
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://github.com/Patchkat/placeTemplater/raw/main/fnalTemplate.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
